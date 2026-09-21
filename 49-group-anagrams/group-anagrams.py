9c03509272de6428a0ed0d66c6081b1611723a1f
Zaara Khan <zaarakhan29052007@gmail.com>
2026-09-21 18:35:47 +0530

    Time: 22 ms (51.09%) | Memory: 15.9 MB (97.94%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,12 +1,11 @@
+from collections import defaultdict
 class Solution(object):
     def groupAnagrams(self, strs):
-        groups = {}
+        groups = defaultdict(list)
 
         for word in strs:
             key = "".join(sorted(word))
-
-            if key not in groups:
-                groups[key]=[]
             groups[key].append(word) 
+            
         return list(groups.values())
         
